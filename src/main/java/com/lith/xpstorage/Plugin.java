9334b45b25565cb12ba82f0a4daa759963ff1733
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-05 23:54:41 +0300

    feat: repair mending items when shift-clicked consuming player exp event

--- src/main/java/com/lith/xpstorage/Plugin.java
+++ src/main/java/com/lith/xpstorage/Plugin.java
@@ -2,6 +2,7 @@ package com.lith.xpstorage;
 
 import com.lith.lithcore.abstractClasses.MainPlugin;
 import com.lith.lithcore.abstractClasses.PluginConfigManager;
+import com.lith.xpstorage.events.ShiftClickEvent;
 
 public class Plugin extends MainPlugin<PluginConfigManager> {
     public static Plugin plugin;
@@ -9,10 +10,16 @@ public class Plugin extends MainPlugin<PluginConfigManager> {
     public void onEnable() {
         Plugin.plugin = this;
 
+        registerEvents();
+
         Static.log.info("Plugin enabled");
     }
 
     public void onDisable() {
         Static.log.info("Plugin disabled");
+    }
+
+    private void registerEvents() {
+        getServer().getPluginManager().registerEvents(new ShiftClickEvent(), this);
     }
 }
