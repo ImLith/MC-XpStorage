b9e28391bdd7002249a65e175c4a36ac8c3444ae
ImLith <baranauskas.edvinas26@gmail.com>
2024-04-15 00:07:42 +0300

    update: lithcore 0.1.0

--- src/main/java/com/lith/xpstorage/Plugin.java
+++ src/main/java/com/lith/xpstorage/Plugin.java
@@ -1,25 +1,12 @@
 package com.lith.xpstorage;
 
-import com.lith.lithcore.abstractClasses.MainPlugin;
-import com.lith.lithcore.abstractClasses.PluginConfigManager;
+import com.lith.lithcore.abstractClasses.AbstractPlugin;
 import com.lith.xpstorage.events.ShiftClickEvent;
 
-public class Plugin extends MainPlugin<PluginConfigManager> {
-    public static Plugin plugin;
-
-    public void onEnable() {
-        Plugin.plugin = this;
-
-        registerEvents();
-
-        Static.log.info("Plugin enabled");
-    }
-
-    public void onDisable() {
-        Static.log.info("Plugin disabled");
-    }
-
-    private void registerEvents() {
-        getServer().getPluginManager().registerEvents(new ShiftClickEvent(), this);
+@SuppressWarnings("rawtypes")
+public class Plugin extends AbstractPlugin {
+    @Override
+    protected void registerEvents() {
+        registerEvent(new ShiftClickEvent());
     }
 }
--- src/main/java/com/lith/xpstorage/Static.java
+++
@@ -1,8 +0,0 @@
-package com.lith.xpstorage;
-
-import java.util.logging.Logger;
-
-public class Static {
-    public static final String pluginName = "XpStorage";
-    public static final Logger log = Logger.getLogger(Static.pluginName);
-}
